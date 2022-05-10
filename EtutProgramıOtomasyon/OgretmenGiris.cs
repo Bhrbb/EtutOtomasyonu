7e63316e87c72f3656268fa523aaec2f85ee8227
Bahar pehlivan <bhrbzdmr@gmail.com>
2022-05-10 20:08:07 +0300

    test

--- EtutProgramıOtomasyon/OgretmenGiris.cs
+++ EtutProgramıOtomasyon/OgretmenGiris.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -21,8 +22,11 @@ namespace EtutProgramıOtomasyon
             this.kullan = kullan;
         }
         EtutManagercs Em = new EtutManagercs();
-        OgretmenEtutManager oe= new OgretmenEtutManager();
-        
+        OgretmenEtutManager oe = new OgretmenEtutManager();
+        DataContext context = new DataContext();
+        OgretmenManager om = new OgretmenManager();
+
+
         private void OgretmenGiris_Load(object sender, EventArgs e)
         {
             string.Join(",", kullan);
@@ -30,12 +34,49 @@ namespace EtutProgramıOtomasyon
             string k = "";
             foreach (Kullanici item in kullan)
             {
-                 k= item.KullaniciAd;
+                k = item.KullaniciAd;
                 textBox1.Text = k;
             }
             //var kull=kullan.FirstOrDefault(y=>y.KullaniciAd==)
 
             dataGridView1.DataSource = Em.GetAll1(x => x.OgretmenAdSoyad == k);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string k = "";
+            foreach (Kullanici item in kullan)
+            {
+                k = item.KullaniciAd;
+                textBox1.Text = k;
+            }
+            var ders = context.ogretmenler.Where(x => x.OgretmenAdSoyad == k);
+            dataGridView1.DataSource = ders.ToList();
+            int islem = Em.Add(
+           new Etut
+           {
+
+               // Adi = cmbders.Text,
+               // DersAd =context.ogretmenler.Where() ,
+               OgrenciAdSoyad = textBox1.Text,
+               OgretmenAdSoyad = k,
+
+               Tarih = maskedTextBox1.Text,
+
+               Saat = maskedTextBox2.Text
+
+
+           }
+
+           );
+            if (islem > 0)
+            {
+                dataGridView1.DataSource = Em.GetAll();
+                MessageBox.Show("Kayıt başarılı!");
+            }
+            else
+                MessageBox.Show("Kayıt başarısız!");
         }
     }
 }
